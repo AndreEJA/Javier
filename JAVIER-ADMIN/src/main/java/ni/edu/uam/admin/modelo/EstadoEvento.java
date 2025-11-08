5fa7e1da97eb699ae9eead95e1d13e8fe278afa7
sea2horses <thefuzegamer06@gmail.com>
2025-11-08 00:05:50 -0600

    Agregar Voto y VotoDetalle

---
+++ JAVIER-ADMIN/src/main/java/ni/edu/uam/admin/modelo/EstadoEvento.java
@@ -0,0 +1,4 @@
+package ni.edu.uam.admin.modelo;
+
+public enum EstadoEvento {
+}
---
+++ JAVIER-ADMIN/src/main/java/ni/edu/uam/admin/modelo/Facultad.java
@@ -0,0 +1,11 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+package ni.edu.uam.JAVIER-ADMIN.modelo;
+
+@Entity
+@Getter @Setter
+public class Facultad {
+}
